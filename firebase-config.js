f86aed318559ae51fd8ee6c879c40ae66eee05e7
Nadzira <nadzira_22010431@utp.edu.my>
2025-07-17 09:08:18 +0800

    initialization

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIGtYaq-Zu4ZZoySQVU3vyHr87qMm510o",
+  authDomain: "utp-club-website.firebaseapp.com",
+  projectId: "utp-club-website",
+  storageBucket: "utp-club-website.firebasestorage.app",
+  messagingSenderId: "318664826794",
+  appId: "1:318664826794:web:d202623e2a99caaa0a55d3",
+  measurementId: "G-2TQFLZ3B5D"
+};
